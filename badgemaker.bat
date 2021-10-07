98445256e34a1447b6fd7a01bc044e7f0551ccd2
IzzyBells <57155401+IzzyBells@users.noreply.github.com>
2021-10-07 04:56:34 -0500

    Add files via upload

---
+++ badgemaker.bat
@@ -0,0 +1,8 @@
+::Turn of displaying the code on the screen
+@echo off
+
+::Create subdirectory
+if not exist "%1\badges\nul" md "%1\badges"
+
+:: Read all the images from the directory, resize them, save as a 64x64 png in badges subdirectory
+for %%f in (%1\*.png) do ( magick convert "%%f" -resize 64x64 %1\badges\%%~nf.png )
